6e4b38fc5a0e90acb3dd006836881323791a291e
Akash AR <akuaka750@gmail.com>
2025-01-14 17:38:52 +0530

    first commit

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+class CategoryGridItem extends StatelessWidget{
+
+  const CategoryGridItem ({super.key, required this.category});
+
+  final Category category;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            // ignore: deprecated_member_use
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.9)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
+          ),
+          ),
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          )
+    );
+  }
+}
