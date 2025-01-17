c5c3dd2a5280710ebcc7e2679ab954b2a0009f29
Akash AR <akuaka750@gmail.com>
2025-01-17 19:00:54 +0530

    fourth commit

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meals_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget{
@@ -8,6 +9,12 @@ class MealsScreen extends StatelessWidget{
   final String title ;
 
   final List<Meal> meals ;
+
+  void selectMeal(BuildContext context ,  Meal meal){
+    Navigator
+    .of(context)
+    .push(MaterialPageRoute(builder: (ctx)=>MealsDetailsScreen(meal:meal)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +46,9 @@ class MealsScreen extends StatelessWidget{
     if(meals.isNotEmpty){
       content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (context, index) => MealItem(meal: meals[index]),
+      itemBuilder: (context, index) => MealItem(meal: meals[index] , onSelectMeal:(meal){
+        selectMeal(context, meal);
+      } ,),
     );
     }
 
