fc9cfd190d47c044200e4d802627d6289b945069
Akash AR <akuaka750@gmail.com>
2025-01-18 19:32:49 +0530

    fifth commit

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -2,62 +2,75 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
+import 'package:meals_app/widgets/meal_item_trait.dart';
 
-class MealsScreen extends StatelessWidget{
-  const MealsScreen({super.key , required this.title , required this.meals});
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+    required this.onToggleFavorite,
+  });
 
-  final String title ;
+  final String? title;
+  final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
 
-  final List<Meal> meals ;
-
-  void selectMeal(BuildContext context ,  Meal meal){
-    Navigator
-    .of(context)
-    .push(MaterialPageRoute(builder: (ctx)=>MealsDetailsScreen(meal:meal)));
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsDetailsScreen(
+          meal: meal,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget content =  Center(   
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Uh oh nothings here....',
-              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                // ignore: deprecated_member_use
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
-            ),
-            const SizedBox(height:16),
-            Text(
-              'Try selecting a diff category!',
-              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                // ignore: deprecated_member_use
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
-            )
-          ],
-        )
-      );
-    
-     
-
-    if(meals.isNotEmpty){
-      content = ListView.builder(
-      itemCount: meals.length,
-      itemBuilder: (context, index) => MealItem(meal: meals[index] , onSelectMeal:(meal){
-        selectMeal(context, meal);
-      } ,),
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
     );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (meal) {
+            selectMeal(context, meal);
+          },
+        ),
+      );
     }
 
-  return Scaffold(
-    appBar: AppBar(
-      title: Text(title)
+    if (title == null) {
+      return content;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
       ),
       body: content,
-  );
+    );
   }
-  
 }
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -4,13 +4,16 @@ import 'package:flutter/material.dart';
 
 import '../models/category.dart';
 
-class CategoryGridItem extends StatelessWidget{
-
-  const CategoryGridItem ({super.key, required this.category , required this.onSelectCategory});
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
 
   final Category category;
   final void Function() onSelectCategory;
-  
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -20,23 +23,21 @@ class CategoryGridItem extends StatelessWidget{
       child: Container(
         padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          gradient: LinearGradient(
-            colors: [
-              // ignore: deprecated_member_use
-              category.color.withOpacity(0.55),
-              category.color.withOpacity(0.9)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-            ),
-            ),
-            child: Text(
-              category.title,
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.55),
+                category.color.withOpacity(0.9),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
                 color: Theme.of(context).colorScheme.onBackground,
               ),
-            )
+        ),
       ),
     );
   }
